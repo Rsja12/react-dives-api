9ed0c226be771a8c8166978ebc33f753b49c985e
Rsja12 <asocarras10@gmail.com>
2020-07-03 15:53:00 -0400

    finish authController

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,8 +2,26 @@ class Api::V1::AuthController < ApplicationController
 
     skip_before_action :authorized, only: [:create]
 
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
+
     def create
         @user = User.find_by(email: user_login_params[:email])
+        # .authenticate comes from BCrypt
+        if @user && @user.authenticate(user_login_params[:password])\
+            # encode_token comes from ApplicationController
+            token = encode_token({ user_id: @user.id })
+            render json: { user: UserSerializer.new(@user), jwt: token }, status: :accepted
+        else
+            render json: { message: 'Invalid username or password' }, status: :unauthorized
+        end
+    end
+
+    private
+
+    def user_login_params
+        params.require(:user).permit(:email, :password)
     end
 
 end
