bb1abd6a70aa90d67779887796c94ac130ad40d6
Rsja12 <asocarras10@gmail.com>
2020-07-05 15:37:07 -0400

    add associations

--- app/models/divelog.rb
+++ app/models/divelog.rb
@@ -1,2 +1,3 @@
 class Divelog < ApplicationRecord
+    belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    has_many :divelogs
     validates :email, :name, presence: true
     validates :email, uniqueness: true
 end
