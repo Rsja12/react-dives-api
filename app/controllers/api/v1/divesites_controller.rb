6a1b8f68db86dff85e04e6e124d200169d06d054
Rsja12 <asocarras10@gmail.com>
2020-07-05 15:43:08 -0400

    create divesites controller and route

---
+++ app/controllers/api/v1/divesites_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::DivesitesController < ApplicationController
+
+    def index
+        if logged_in?
+          @secrets = current_user.secrets
+          render json: @secrets, status: :ok
+        else
+          render json: { error: 'not logged in', status: :unauthorized }
+        end
+    end
+    
+end
