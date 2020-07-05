2a398f709b7e41752525fe2d2456b72e21ab91e5
Rsja12 <asocarras10@gmail.com>
2020-07-05 17:37:53 -0400

    update db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,5 +4,5 @@
 
 User.create(name: 'John', email: 'userOne@email.com', password: 'password')
 
-Divelog.create(user_id: 1, location: 'Grand Cayman', divesite: 'North Wall', max_depth: 110, bottom_time: 25, visibility: 100, weight: 8, notes: 'Great dive with awesome visibility. There was a strong current in the first 40ft, but it calmed down after that. Saw lots of sharks.')
-Divelog.create(user_id: 1, location: 'Grand Cayman', divesite: 'Kittiwake', max_depth: 55, bottom_time: 45, visibility: 100, weight: 8, notes: 'Awesome wreck dive. Very disorienting inside of the wreck')
+Divelog.create(user_id: 1, location: 'Grand Cayman', divesite: 'North Wall', max_depth: 110, bottom_time: 25, visibility: 100, water_temp: 83, weight: 8, notes: 'Great dive with awesome visibility. There was a strong current in the first 40ft, but it calmed down after that. Saw lots of sharks.')
+Divelog.create(user_id: 1, location: 'Grand Cayman', divesite: 'Kittiwake', max_depth: 55, bottom_time: 45, visibility: 100, water_temp: 80, weight: 8, notes: 'Awesome wreck dive. Very disorienting inside of the wreck')
